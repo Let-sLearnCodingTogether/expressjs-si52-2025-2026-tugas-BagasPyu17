b35b42eeb51909a96c6c1ff1f23ab3e5198242e1
BagasPyu17 <adrianusbagaspyu1@gmail.com>
2025-10-26 22:08:29 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import api from "./route/api.js";
+import database from "./config/database.js";
+import passport from "passport";
+
+const app = express();
+app.use(express.json())
+
+app.use(passport.initialize())
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.use("/api", api)
+
+app.listen(3000, () => {
+    database()
+    console.log(`Aplikasi berjalan di http://localhost:3000`);
+})
