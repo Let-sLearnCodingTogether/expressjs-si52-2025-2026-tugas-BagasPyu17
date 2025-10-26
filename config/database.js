e2bc859ab55e40022714ff1a47fd51ebee04b640
BagasPyu17 <adrianusbagaspyu1@gmail.com>
2025-10-26 22:08:33 +0700

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import mongoose, { syncIndexes } from "mongoose";
+
+export const database = async () => {
+    try {
+        console.log("Starting connection to mongodb...");
+
+        const response = await mongoose.connect("mongodb://127.0.0.1:27017/bagas?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
+
+        console.log(`Connected succesfully to mongodb at : ${response.connection.host}`);
+
+    } catch (error) {
+        console.log("Mongodb connection failed");
+
+        process.exit(1)
+    }
+}
+
+export default database;
